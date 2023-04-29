4f2065792479cba5de3e8ed6cc6763476463602f
koman4 <66549200+koman4@users.noreply.github.com>
2023-04-29 18:16:24 +0300

    добавлено решение на ДЗ 12.7.3

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import bs4
+import numpy as np
+
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = 10000
+
+A = per_cent.get('ТКБ')
+bank1 = money + (money/100*A)
+B = per_cent.get('СКБ')
+bank2 = money + (money/100*B)
+C = per_cent.get('ВТБ')
+bank3 = money + (money/100*C)
+D = per_cent.get('СБЕР')
+bank4 = money + (money/100*D)
+deposit = [bank1, bank2, bank3, bank4]
+max_deposit = max(deposit)
+print("Максимальная сумма, которую вы можете заработать — " + str(int(max_deposit)) + ". " )
